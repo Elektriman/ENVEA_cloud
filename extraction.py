6455fa865c48890ab795dfaefb854fae7462de90
Julien Crambes <juju.crambes@gmail.com>
2023-06-28 11:19:47 +0200

    refactor extraction.py
    Edited to fit better the python variable naming conventions

--- extraction.py
+++ extraction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import datetime
-from ENVEA import ENVEA
+from ENVEA import Envea
 from pandas import json_normalize
 from logging_config import logger
 
@@ -14,7 +14,7 @@ workingDataFile = f"csv_data\\data_{datetime.datetime.now().strftime('%Y')}.csv"
 with open("last.txt", "r") as f:
     last = pd.to_datetime(datetime.datetime.fromisoformat(f.read()), unit='ns', utc=True)
 
-LAST = pd.read_csv("csv_data/last_data.csv")
+most_recent = pd.read_csv("csv_data/last_data.csv")
 
 # fetching either 1h data or data since last execution
 if now - last > datetime.timedelta(7):
@@ -23,61 +23,61 @@ else:
     data_kwargs = {"lastHours": 168}
 
 # call the API
-with ENVEA() as envea:
-    data_list = envea.retrieve(ressource="restricted/v1/data", **data_kwargs)
-    measures_list = envea.retrieve('restricted/v1/measures')
-    sites_list = envea.retrieve('restricted/v1/sites')
+with Envea() as envea_dialoger:
+    data_list = envea_dialoger.retrieve(ressource="restricted/v1/data", **data_kwargs)
+    measures_list = envea_dialoger.retrieve('restricted/v1/measures')
+    sites_list = envea_dialoger.retrieve('restricted/v1/sites')
 
 # combine batched requests
 if os.path.exists(os.path.join(os.path.dirname(__file__), workingDataFile)):
-    DATA = pd.read_csv(workingDataFile, index_col=0)
+    data_points = pd.read_csv(workingDataFile, index_col=0)
 else:
-    DATA = pd.DataFrame(columns=["date", "id", "state", "validated", "value"])
+    data_points = pd.DataFrame(columns=["date", "id", "state", "validated", "value"])
 
 data_df_list = [json_normalize(d.json()["data"], "base", meta="id") for d in data_list]
 UPDATED = pd.concat([pd.DataFrame(columns=["date", "id", "state", "validated", "value"])] + data_df_list)
 
 UPDATED = UPDATED[UPDATED["id"].str.contains('H2S')]
 
-MEASURES = pd.concat([json_normalize(m.json(), "measures") for m in measures_list])
-SITES = pd.concat([json_normalize(s.json(), "sites") for s in sites_list])
+measures = pd.concat([json_normalize(m.json(), "measures") for m in measures_list])
+sites = pd.concat([json_normalize(s.json(), "sites") for s in sites_list])
 
 # cast datetime data
 UPDATED["date"] = pd.to_datetime(UPDATED["date"], utc=True)
 
-LAST = LAST.astype(UPDATED.dtypes)
+most_recent = most_recent.astype(UPDATED.dtypes)
 
 for i, row in UPDATED.dropna().iterrows():
     ID = row['id']
-    j = LAST.query("id==@ID").index.values[0]
-    if LAST.loc[j, "date"] < row["date"] or pd.isna(LAST.loc[j, "date"]):
-        LAST.iloc[j] = row.values
+    j = most_recent.query("id==@ID").index.values[0]
+    if most_recent.loc[j, "date"] < row["date"] or pd.isna(most_recent.loc[j, "date"]):
+        most_recent.iloc[j] = row.values
 
 UPDATED["date"] = UPDATED["date"].dt.strftime("%d/%m/%Y %H:%M:%S")
 
-DATA = pd.concat([DATA, UPDATED])
-DATA.drop_duplicates(inplace=True)
+data_points = pd.concat([data_points, UPDATED])
+data_points.drop_duplicates(inplace=True)
 
 for col in ["lastDataDate", "startDate", "stopDate"]:
-    MEASURES[col] = pd.to_datetime(MEASURES[col]).dt.strftime("%d/%m/%Y %H:%M:%S")
+    measures[col] = pd.to_datetime(measures[col]).dt.strftime("%d/%m/%Y %H:%M:%S")
 
 for col in ["startDate", "stopDate"]:
-    SITES[col] = pd.to_datetime(SITES[col]).dt.strftime("%d/%m/%Y %H:%M:%S")
+    sites[col] = pd.to_datetime(sites[col]).dt.strftime("%d/%m/%Y %H:%M:%S")
 
 # order the columns for PowerBI
-DATA = DATA.reindex(sorted(DATA.columns), axis=1)
-MEASURES = MEASURES.reindex(sorted(MEASURES.columns), axis=1)
-SITES = SITES.reindex(sorted(SITES.columns), axis=1)
+data_points = data_points.reindex(sorted(data_points.columns), axis=1)
+measures = measures.reindex(sorted(measures.columns), axis=1)
+sites = sites.reindex(sorted(sites.columns), axis=1)
 
 add_header = not os.path.exists(
     os.path.dirname(__file__) + "\\" + f"csv_data\\data_{datetime.datetime.now().strftime('%Y')}.csv")
 
 try:
     # write files to csv
-    DATA.to_csv(f"csv_data\\data_{datetime.datetime.now().strftime('%Y')}.csv", mode="a", header=add_header)
-    MEASURES.to_csv(f"csv_data\\measures.csv")
-    SITES.to_csv(f"csv_data\\sites.csv")
-    LAST.to_csv("csv_data\\last_data.csv", index=False)
+    data_points.to_csv(f"csv_data\\data_{datetime.datetime.now().strftime('%Y')}.csv", mode="a", header=add_header)
+    measures.to_csv(f"csv_data\\measures.csv")
+    sites.to_csv(f"csv_data\\sites.csv")
+    most_recent.to_csv("csv_data\\last_data.csv", index=False)
 except Exception as e:
     logger.exception(e)
 else:
